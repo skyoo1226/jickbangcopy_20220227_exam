67b184e219a066f10ef11264142a2dbe1993339f
skyoo1226 <you1226@naver.com>
2022-03-03 04:09:22 +0900

    기본 뉴프로젝트 준비 -방 한칸 보여주는 UI -Data 클래스 생성 -> 4개의 멤버변수 지정을 함 -받아서 뿌려줄 어댑터 클래스를 추가고, ArrayAdapter로 RoomData를 뿌려주게 상속을 받게 함. -ArrayAdapter는 기본 생성자를 지원하지 않음.(에러 메세지...) -RoomAdapter에서 변수를 지정해서 ArrayAdapter의 변수로 지정하면 에러 메세지 없어짐.=> 어댑터 클래스의 생성자를 수정하여 부모 클래스의 생성자에 변수를 전달 - 어댑터 클레스의 겟뷰getView 함수 수정해서 그려둔 list_item xml화면을 inflate해서 객체로 만들어 리턴 함.

--- app/src/main/java/com/skyoo/jickbangcopy_20220227_exam/adapters/RoomAdapter.kt
+++ app/src/main/java/com/skyoo/jickbangcopy_20220227_exam/adapters/RoomAdapter.kt
@@ -1,7 +1,12 @@
 package com.skyoo.jickbangcopy_20220227_exam.adapters
 
 import android.content.Context
+import android.text.Layout
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.ArrayAdapter
+import com.skyoo.jickbangcopy_20220227_exam.R
 import com.skyoo.jickbangcopy_20220227_exam.datas.RoomData
 
 class RoomAdapter(
@@ -9,5 +14,22 @@ class RoomAdapter(
     val resId: Int,
     val mList: ArrayList<RoomData>
 ) : ArrayAdapter<RoomData>(mContext, resId, mList) {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+
+        var tempRow = convertView
+
+        if (tempRow == null) {
+            tempRow = LayoutInflater.from(mContext).inflate(R.layout.room_list_item, null)
+        }
+
+        val  row = tempRow!!
+
+        return row
+
+
+    }
+
+
 
 }
