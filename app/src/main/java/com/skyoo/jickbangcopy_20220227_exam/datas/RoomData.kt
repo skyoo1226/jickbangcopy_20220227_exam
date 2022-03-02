34bd5c0d5fa3ae8939b96c7943abaf686ebd9e4c
skyoo1226 <you1226@naver.com>
2022-03-03 03:22:43 +0900

    기본 뉴프로젝트 준비 -방 한칸 보여주는 UI -Data 클래스 생성 -> 4개의 멤버변수 지정을 함

---
+++ app/src/main/java/com/skyoo/jickbangcopy_20220227_exam/datas/RoomData.kt
@@ -0,0 +1,14 @@
+package com.skyoo.jickbangcopy_20220227_exam.datas
+
+// 하나의 방(class)에는 4가지 정보를 넣을 예정, 가격,행정구역,층수,설명문구 - 멤버변수로 만듬.
+
+
+class RoomData(
+    val price : Int,
+    val address : String,
+    val floor : Int,
+    val description : String,
+) {
+
+
+}
