82ed978b38d56b4e83a7e7072c744d9efd3f3051
skyoo1226 <you1226@naver.com>
2022-03-03 04:21:20 +0900

    방 목록을 직접 타이핑해서(임시로) 올려둠.

--- app/src/main/java/com/skyoo/jickbangcopy_20220227_exam/MainActivity.kt
+++ app/src/main/java/com/skyoo/jickbangcopy_20220227_exam/MainActivity.kt
@@ -2,10 +2,29 @@ package com.skyoo.jickbangcopy_20220227_exam
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.skyoo.jickbangcopy_20220227_exam.datas.RoomData
 
 class MainActivity : AppCompatActivity() {
+
+    val mRoomList = ArrayList<RoomData>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        mRoomList.add(RoomData(8000, "서울시 동대문구", 7, "1번째 방입니다."))
+        mRoomList.add(RoomData(9000, "서울시 동대문구", 8, "2번째 방입니다."))
+        mRoomList.add(RoomData(10000, "서울시 동대문구", 9, "3번째 방입니다."))
+        mRoomList.add(RoomData(11000, "서울시 동대문구", 10, "4번째 방입니다."))
+        mRoomList.add(RoomData(12000, "서울시 동대문구", 11, "5번째 방입니다."))
+        mRoomList.add(RoomData(13000, "서울시 동대문구", 12, "6번째 방입니다."))
+        mRoomList.add(RoomData(16000, "서울시 동대문구", 20, "7번째 방입니다."))
+        mRoomList.add(RoomData(5000, "서울시 동대문구", 2, "8번째 방입니다."))
+        mRoomList.add(RoomData(4000, "서울시 동대문구", 1, "9번째 방입니다."))
+        mRoomList.add(RoomData(6000, "서울시 동대문구", 4, "10번째 방입니다."))
+        mRoomList.add(RoomData(19000, "서울시 동대문구", 7, "11번째 방입니다."))
+        mRoomList.add(RoomData(27000, "서울시 동대문구", 22, "12번째 방입니다."))
+        mRoomList.add(RoomData(20000, "서울시 동대문구", -1, "13번째 방입니다."))
+
     }
 }
