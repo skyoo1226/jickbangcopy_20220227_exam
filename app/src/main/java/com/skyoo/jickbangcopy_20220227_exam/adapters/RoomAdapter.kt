72c3c528525aee23313d47e48e87b796ead48387
skyoo1226 <you1226@naver.com>
2022-03-03 03:45:25 +0900

    기본 뉴프로젝트 준비 -방 한칸 보여주는 UI -Data 클래스 생성 -> 4개의 멤버변수 지정을 함 -받아서 뿌려줄 어댑터 클래스를 추가고, ArrayAdapter로 RoomData를 뿌려주게 상속을 받게 함. -ArrayAdapter는 기본 생성자를 지원하지 않음.(에러 메세지...) -RoomAdapter에서 변수를 지정해서 ArrayAdapter의 변수로 지정하면 에러 메세지 없어짐.

--- app/src/main/java/com/skyoo/jickbangcopy_20220227_exam/adapters/RoomAdapter.kt
+++ app/src/main/java/com/skyoo/jickbangcopy_20220227_exam/adapters/RoomAdapter.kt
@@ -1,7 +1,12 @@
 package com.skyoo.jickbangcopy_20220227_exam.adapters
 
+import android.content.Context
 import android.widget.ArrayAdapter
 import com.skyoo.jickbangcopy_20220227_exam.datas.RoomData
 
-class RoomAdapter : ArrayAdapter<RoomData>() {
+class RoomAdapter(
+    val mContext: Context,
+    val resId: Int,
+    val mList: ArrayList<RoomData>
+) : ArrayAdapter<RoomData>(mContext, resId, mList) {
 }
