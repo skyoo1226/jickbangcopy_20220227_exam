311713b822ca38ef4f92abe423f54d1ce87ec956
skyoo1226 <you1226@naver.com>
2022-03-03 07:42:09 +0900

    가공된(억잔위 표시 등) 가격 표시와 가공된(층수표현) 각 층수를 함수로 계산된 값을 가져오고, 방설명과 주소는 그대로 가져옴.

--- app/src/main/java/com/skyoo/jickbangcopy_20220227_exam/ViewRoomDetailActivity.kt
+++ app/src/main/java/com/skyoo/jickbangcopy_20220227_exam/ViewRoomDetailActivity.kt
@@ -3,6 +3,7 @@ package com.skyoo.jickbangcopy_20220227_exam
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.skyoo.jickbangcopy_20220227_exam.datas.RoomData
+import kotlinx.android.synthetic.main.activity_view_room_detail.*
 
 class ViewRoomDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -11,7 +12,13 @@ class ViewRoomDetailActivity : AppCompatActivity() {
 
         val roomData = intent.getSerializableExtra("room") as RoomData
 
-
+//        가공된 가격 표시로 가져오기
+        txtPrice.text = roomData.getFormattedPrice()
+//        설명관 주소는 그대로 가져 옴
+        txtDescription.text = roomData.description
+        txtAddress.text = roomData.address
+//        가공된 10층-> 반지하, 지하 등 문구로 변환된 자료 가져오기
+        txtFloor.text = roomData.getFormattedFloor()
 
 
     }
